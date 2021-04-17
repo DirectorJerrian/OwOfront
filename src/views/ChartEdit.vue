9b299830717eaf2d7fb799b941f61596691bd7f1
rx <181250120@smail.nju.edu.cn>
2021-04-17 21:32:43 +0800

    fix:读取三元组

--- src/views/ChartEdit.vue
+++ src/views/ChartEdit.vue
@@ -698,24 +698,31 @@
           this.nodes=this.chartData.nodes;
           this.links=this.chartData.links;
           //获取categories
-          var myCategories=[];
-          var numOfCategories=0;
+          // var myCategories=[];
+          // var numOfCategories=0;
+          // for(var i=0;i<this.chartData.nodes.length;i++){
+          //   if(myCategories.indexOf(this.chartData.nodes[i].category)>-1)
+          //     continue;
+          //   else{
+          //     myCategories.push(this.chartData.nodes[i].category);
+          //     // myCategories.push({ name: "" });
+          //     // myCategories[numOfCategories].name= this.chartData.nodes[i].category+" class";
+          //     numOfCategories+=1;
+          //   }
+          // }
+          // // this.categories=myCategories;
+          // myCategories=myCategories.sort();
+          // this.categories=[];
+          // for(var i=0;i<numOfCategories;i++){
+          //   this.categories.push({ name: "" });
+          //   this.categories[i].name= myCategories[i]+" class";
+          // }
+          this.categories=[];
           for(var i=0;i<this.chartData.nodes.length;i++){
-            if(myCategories.indexOf(this.chartData.nodes[i].category+"")>-1)
-              continue;
-            else{
-              myCategories.push(this.chartData.nodes[i].category+"");
-              // myCategories.push({ name: "" });
-              // myCategories[numOfCategories].name= this.chartData.nodes[i].category+" class";
-              numOfCategories+=1;
-            }
-          }
-          // this.categories=myCategories;
-          myCategories=myCategories.sort();
-          for(var i=0;i<numOfCategories;i++){
             this.categories.push({ name: "" });
-            this.categories[i].name= myCategories[i]+" class";
+            this.categories[i].name= " class";
           }
+          console.log(this.categories);
           this.option.title.text=this.chartData.title;
           this.isChartFixed=this.chartData.isChartFixed;
           if(this.isChartFixed){
