dd232c3616de66456a36bc67307255ce7b16b161
DirectorJerrian <181250192@smail.nju.edu.cn>
2021-04-18 09:50:26 +0800

    feat: implement file update

--- src/store/modules/chart.js
+++ src/store/modules/chart.js
@@ -2,13 +2,14 @@ import router from '@/router'
 import { resetRouter } from '@/router'
 import {Message} from 'element-ui'
 import {saveChartAPI} from "../../api/chart";
+import {getToken, setToken, removeToken} from '../../utils/auth'
 
 const getDefaultState=()=>{
   return {
     chartData: null,
     chartList:[],
   }
-}
+};
 
 const chart={
   state:getDefaultState(),
@@ -21,14 +22,19 @@ const chart={
     }
   },
   actions:{
-    addChart: async ({dispatch,commit},data)=>{
-      const res= await saveChartAPI(data);
+    addChart: async ({dispatch,state,commit},data)=>{
+      const fileData={
+        id: getToken(),
+        jsonFile: data.jsonFile,
+        imgFile: data.imgFile
+      };
+      const res= await saveChartAPI(fileData);
       if(res){
-
+        return true
       }
     },
   }
-}
+};
 
 export default chart
 
--- src/views/ChartEdit.vue
+++ src/views/ChartEdit.vue
@@ -1399,12 +1399,12 @@
           }
           return chartToBeSaved;
         },
-        saveChartClick(){
-          var chartToBeSaved=this.getChartData();
+        async saveChartClick() {
+          var chartToBeSaved = this.getChartData();
           //测试json文件生成
-          var jsonData=JSON.stringify(chartToBeSaved,undefined,4);
-          const jsonFile=new Blob([jsonData],{type:'text/json'});
-          const imgFile=this.getChartImgFile();
+          var jsonData = JSON.stringify(chartToBeSaved, undefined, 4);
+          const jsonFile = new Blob([jsonData], {type: 'text/json'});
+          const imgFile = this.getChartImgFile();
           // var e = document.createEvent('MouseEvents');
           // var a = document.createElement('a')
           // a.download = "chart.json";
@@ -1413,12 +1413,19 @@
           // e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
           // a.dispatchEvent(e);
           //TODO 数据库保存
-          if(this.isChartFixed){
-            this.successNotice("保存成功!（已经保存布局）")
+          const data = {
+            jsonFile: jsonFile,
+            imgFile: imgFile
+          };
+          if ( await this.addChart(data)) {
+            if (this.isChartFixed) {
+              this.successNotice("保存成功!（已经保存布局）")
+            } else {
+              this.successNotice("保存成功!（未保存布局）")
+            }
           }else{
-            this.successNotice("保存成功!（未保存布局）")
+            this.warningNotice("保存失败！")
           }
-
         },
         //点击事件
         chartClick(param){
