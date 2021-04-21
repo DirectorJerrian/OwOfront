900016aa96475458031f6b495deec01ebc5aa9d3
DirectorJerrian <181250192@smail.nju.edu.cn>
2021-04-21 14:14:15 +0800

    debug: add proxy

--- config/index.js
+++ config/index.js
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '47.118.57.5', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
@@ -52,7 +52,15 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
-
+    proxyTable: {
+      '/aliyun': {
+        target: 'http://software-engineering-iii.oss-cn-hangzhou.aliyuncs.com',//设置你调用的接口域名和端口号 别忘了加http
+        changeOrigin: true,    //這裡true表示实现跨域
+        pathRewrite: {
+          '^/aliyun': '/'//这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
+        }
+      }
+    },
     /**
      * Source Maps
      */
